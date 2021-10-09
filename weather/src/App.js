2b1de9af2b72f3c034d7ee1feb8d73e7093d29a2
ordegani <oradeganii@gmail.com>
2021-10-10 02:17:52 +0300

    changes

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
+import {useSelector} from 'react-redux';
 
+const locationReducer = useSelector(state => state.location)
 function App() {
+  <div className="app">
+    <h1>{locationReducer}</h1>
+  </div>
 }
 
 export default App;
