a6cf5bc995236883f532ebd625ebace11d9e91ba
ordegani <oradeganii@gmail.com>
2021-10-09 16:37:47 +0300

    first commit

--- weather/src/reducers/countryReducer.js
+++ weather/src/reducers/countryReducer.js
@@ -1,16 +1,14 @@
-import { createStore } from 'redux'
+import { createStore } from "redux";
 
 function countryReducer(state = { value: "" }, action) {
   switch (action.type) {
-    case 'country/query':
-      return { value: {query} }
-
+    case "country/query":
+      return { value: { query } };
   }
 }
 
-let store = createStore(countryReducer)
+let store = createStore(countryReducer);
 
-store.subscribe(() => console.log(store.getState()))
+store.subscribe(() => console.log(store.getState()));
 
-store.dispatch({ type: 'country/query' })
-
+store.dispatch({ type: "country/query" });
