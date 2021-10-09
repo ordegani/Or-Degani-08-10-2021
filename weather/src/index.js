e16bf3a563d4eb9c6c95823c465789e206821439
ordegani <oradeganii@gmail.com>
2021-10-10 02:12:41 +0300

    changes

--- weather/src/index.js
+++ weather/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { createStore } from "redux";
 import allReducers from "./reducers";
 import reportWebVitals from "./reportWebVitals";
+import {Provider} from 'react-redux';
 
 const store = createStore(
   allReducers,
@@ -12,13 +13,10 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
