a2b8356d9189c94c1966130f9599079a960b2dc9
ordegani <oradeganii@gmail.com>
2021-10-10 00:22:10 +0300

    changes

--- weather/src/index.js
+++ weather/src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {createStore} from 'redux';
-import allReducers from './reducers';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import allReducers from "./reducers";
+import reportWebVitals from "./reportWebVitals";
 
-const store = createStore(allReducers,
+const store = createStore(
+  allReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
