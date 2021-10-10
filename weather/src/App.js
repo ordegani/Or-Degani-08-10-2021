925d72077526beb0c5e7cb4d9e914448f721480f
ordegani <oradeganii@gmail.com>
2021-10-10 17:03:13 +0300

    changes

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import {useSelector} from 'react-redux';
-import savedReducer from './reducers/savedReducer';
 
 function App() {
-  const savedReducer = useSelector(state => state.savedReducer);
+  const locationReducer = useSelector(state => state.locationReducer);
   return(
   <div className="app">
-    <h1>{savedReducer}</h1>
+    <h1>{locationReducer}</h1>
   </div>
   );
 }
