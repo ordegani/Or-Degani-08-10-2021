897f0aa260b77c1884f51f10983c6e3e7c9bc675
ordegani <oradeganii@gmail.com>
2021-10-14 16:17:37 +0300

    changes

--- weather/src/reducers/index.jsx
+++ weather/src/reducers/index.jsx
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
 import  locationReducer  from './locationReducer';
-import  savedReducer from './savedReducer';
-
+import savedReducer
+ from './savedReducer';
 export default combineReducers({
 locationReducer,
 savedReducer
